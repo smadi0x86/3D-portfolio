3b86c9ab15cbd529bb1982fc9fae2b7bef5bd148
smadi0x86 <smadixd@gmail.com>
2023-03-28 23:36:43 +0300

    Coming soon constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -91,18 +91,14 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Coming Soon",
+    company_name: "COMPANY NAME",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
+    date: "DATE",
+    points: ["INFO"],
   },
+  /*
   {
     title: "React Native Developer",
     company_name: "Tesla",
@@ -142,6 +138,7 @@ const experiences = [
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
+*/
 ];
 
 const projects = [
