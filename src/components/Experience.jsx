5a609245eb612588bfbddee117a94ade8d9e59b1
smadi0x86 <smadixd@gmail.com>
2023-06-29 22:04:40 +0300

    commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -20,7 +20,7 @@ const ExperienceCard = ({ experience }) => {
     <VerticalTimelineElement
       contentStyle={{
         background: "#1d1836",
-        color: "#fff",
+        color: "##808080",
       }}
       contentArrowStyle={{ borderRight: "7px solid  #232631" }}
       date={experience.date}
