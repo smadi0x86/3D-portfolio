672de9d74c8b7c85842b9883d70524e4d6b78fe2
smadi0x86 <smadixd@gmail.com>
2023-07-07 14:25:33 +0300

    reduced stars count to improve performance

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 const Stars = (props) => {
   const ref = useRef();
   const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(3500), { radius: 1.5 })
+    random.inSphere(new Float32Array(1000), { radius: 1.5 })
   );
 
   useFrame((state, delta) => {
